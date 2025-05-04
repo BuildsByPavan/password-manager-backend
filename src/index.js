52a643591c9857bc67ecfdd9f520200282052e91
Dasari Pavan Kumar <pavankumardasari8639@gmail.com>
2025-05-05 01:13:16 +0530

    updated cross orign method

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173', // allow only your React frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api', authRoutes);
